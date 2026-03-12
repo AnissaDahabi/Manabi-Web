b571f59d485b7bdfd74ac9668cd515cf813cfef9
Anissa Dahabi <anissa.Dahabi@gmail.com>
2026-03-12 15:05:19 +0100

    initial commit

---
+++ views/login.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<div class="login-container">
+    <h2>Connexion</h2
+    <form action="controllers/login_controller.php" method="POST">
+        <div>
+            <label>Email:</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label>Mot de passe:</label>
+            <input type="password" name="password">
+        </div>
+        <div>
+            <input type="submit" value="Connexion">
+        </div>
+    </form>
+</div>
+</body>
+</html>
